59253661f9fcc8b1034d2d3c9813ba83bd9f31fb
User <Neirox>
2024-02-10 18:28:18 +0300

    directory_update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxtjs/google-fonts', 'nuxt-icon'],
+  modules: ['@nuxt/image', '@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxtjs/google-fonts', 'nuxt-icon', '@nuxt/ui'],
   googleFonts: {
     families: {
       Pacifico: true,
